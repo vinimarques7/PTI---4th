506270bb5c8e1d692c0d63b5b9c50f195514c6c3
Vinícius Marques <viniciusboz1000@outlook.com>
2023-11-17 03:39:20 -0300

    Images dimensioned

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     var contentContainer = document.getElementById('welcome');
-    if (contentContainer){
+    if (contentContainer) {
         contentContainer.classList.add('visible');
     }
-});
 
-document.addEventListener('DOMContentLoaded', function() {
     var contentSearch = document.getElementById('search');
-    if (contentSearch){
+    if (contentSearch) {
         contentSearch.classList.add('visible');
     }
-})
+
+    var artistsImages = document.getElementById('art');
+    if (artistsImages) {
+        var imgElements = artistsImages.querySelectorAll('.a-img');
+        imgElements.forEach(function(img) {
+            img.classList.add('visible');
+        });
+    }
+});
