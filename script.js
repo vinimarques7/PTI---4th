a3b4fd0c000c393eafc3013fe79982fa80acebd6
Vinícius Marques <viniciusboz1000@outlook.com>
2023-11-17 02:38:25 -0300

    Main screen done

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var contentContainer = document.getElementById('welcome');
+    if (contentContainer){
+        contentContainer.classList.add('visible');
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var contentSearch = document.getElementById('search');
+    if (contentSearch){
+        contentSearch.classList.add('visible');
+    }
+})
